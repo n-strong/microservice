087a7f9de8b104793a8e6ff50c8475986653d0f3
Noah Strong <stronoah@oregonstate.edu>
2024-03-11 18:45:03 -0700

    webpage will now display user blog posts

--- views/blog_post.py
+++ views/blog_post.py
@@ -16,6 +16,12 @@ load_dotenv()
 
 dbURI = os.environ['LOGININFO']
 
+# define MongoDB client
+client = pymongo.MongoClient(dbURI)
+
+# define MongoDB collection 
+collection = client.test['test_post'] # test attribute is database and ['test_post'] is collection within database
+
 # handle requests from frontend
 @app.route('/blog_post', methods=['POST'])
 @cross_origin(origins=['http://localhost:3000'])
@@ -33,8 +39,9 @@ def blog_post():
    
    # try posting data to MongoDB and handle any errors 
     try:
-        post(user_dict)
+        post(user_input)
         print('Posted user post to database')
+
     
     except Exception as e:
         print('Unable to make user post at this time because: %s\n', e)
@@ -43,15 +50,24 @@ def blog_post():
 
 # function to handle post requests for MongoDB
 def post(blog_post):
-    # define MongoDB client
-    client = pymongo.MongoClient(dbURI)
-
-    # define MongoDB collection 
-    collection = client.test['test_post'] # test attribute is database and ['test_post'] is collection within database
-
     # inserts document into database collection 
     collection.insert_one(blog_post)
+ 
+
+@app.route('/get_blog_posts', methods=['POST'])
+@cross_origin(origins=['http://localhost:3000'])
+def fetch():
+    username = request.json['username']
+    posts = get_blog_posts(username)
+    return jsonify({'posts': posts})
+   
+def get_blog_posts(username):
+    docs = collection.find({'username': username})
+   
+    # unpack docs object and store 'posts' in list 
+    posts = [doc['post'] for doc in docs]
     
+    return posts
 
 if __name__ == '__main__':
     app.run(debug=True)
