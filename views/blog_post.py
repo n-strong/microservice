9e2ec6e9b2421843672c214b6ee22f0a92b86f4c
Noah Strong <stronoah@oregonstate.edu>
2024-03-11 18:47:57 -0700

    cleaned up blog_post()

--- views/blog_post.py
+++ views/blog_post.py
@@ -26,27 +26,19 @@ collection = client.test['test_post'] # test attribute is database and ['test_po
 @app.route('/blog_post', methods=['POST'])
 @cross_origin(origins=['http://localhost:3000'])
 def blog_post():
-    
-    # define post container
-    user_dict = {}
 
     # post data from front end
     user_input = request.json 
-    username = user_input['username'] 
-  
-    # username as key and post data as value
-    user_dict[username] = user_input['post']
    
    # try posting data to MongoDB and handle any errors 
     try:
         post(user_input)
         print('Posted user post to database')
 
-    
     except Exception as e:
         print('Unable to make user post at this time because: %s\n', e)
    
-    return jsonify(user_dict) # send signal to webpage to reload after blog post received
+    return jsonify(user_input) 
 
 # function to handle post requests for MongoDB
 def post(blog_post):
